a37078b8612bdd6c1fa963471655801456f7e625
TheA4Paper <thongnoknaravish@gmail.com>
2024-04-03 21:55:57 +0700

    Add wlogout

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -sfn $DPATH/hypr ~/.config/hypr
 ln -sfn $DPATH/kitty ~/.config/kitty
 ln -sfn $DPATH/waybar ~/.config/waybar
 ln -sfn $DPATH/scripts ~/.config/scripts
+ln -sfn $DPATH/wlogout ~/.config/wlogout
 ln -sfn $DPATH/bash/bashrc ~/.bashrc
 mkdir -p ~/Pictures/wallpapers/
 ln -sfn $DPATH/wallpapers ~/Pictures/wallpapers/hypr
