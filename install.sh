9d0a07bd54873d902a29ca37dfe0e1e621599c26
TheA4Paper <thongnoknaravish@gmail.com>
2024-04-03 19:53:26 +0700

    Move scripts location

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ DPATH=$(dirname "$SCRIPT")
 ln -sfn $DPATH/hypr ~/.config/hypr
 ln -sfn $DPATH/kitty ~/.config/kitty
 ln -sfn $DPATH/waybar ~/.config/waybar
+ln -sfn $DPATH/scripts ~/.config/scripts
 ln -sfn $DPATH/bash/bashrc ~/.bashrc
 mkdir -p ~/Pictures/wallpapers/
 ln -sfn $DPATH/wallpapers ~/Pictures/wallpapers/hypr
